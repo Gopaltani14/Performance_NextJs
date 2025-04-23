38bc2e5b9ca76dc303b3a29eaa994cad51c1ca0d
gopaltani14 <gopaltani14@gmail.com>
2025-04-23 01:42:17 -0700

    fix

--- src/app/images/page.jsx
+++ src/app/images/page.jsx
@@ -7,7 +7,12 @@ function Images() {
     <div>
       Images
       <div>
-        <Image src="/20mb1.jpg" alt="image1" width={500} height={500} />
+        <Image
+          src="https://examplefile.com/image/jpg/20-mb-jpg"
+          alt="image1"
+          width={500}
+          height={500}
+        />
         <Image src="/20mb2.jpg" alt="image2" width={500} height={500} />
         <Image src="/20mb3.jpg" alt="image3" width={500} height={500} />
       </div>
