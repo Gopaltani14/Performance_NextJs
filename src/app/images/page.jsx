6a4d4c2be286d6008fa42e302c2c0291dc249c15
gopaltani14 <gopaltani14@gmail.com>
2025-04-24 07:38:36 -0700

    fix

--- src/app/images/page.jsx
+++ src/app/images/page.jsx
@@ -4,17 +4,17 @@ import React from "react";
 
 function Images() {
   return (
-    <div
-      style={{
-        marginTop: "1rem",
-        display: "flex",
-        gap: "1rem",
-        justifyContent: "space-evenly",
-        flexWrap: "wrap",
-      }}
-    >
-      <div>
-        <Image src="/20mb.jpg" alt="image1" width={300} height={300} />
+    <div>
+      <div
+        style={{
+          marginTop: "1rem",
+          display: "flex",
+          gap: "1rem",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+        }}
+      >
+        <Image src="/20mb1.jpg" alt="image1" width={300} height={300} />
         <Image src="/20mb2.jpg" alt="image2" width={300} height={300} />
         <Image src="/20mb3.jpg" alt="image3" width={300} height={300} />
       </div>
